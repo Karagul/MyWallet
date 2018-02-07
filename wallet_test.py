74083f274abfcec5a3c10a7bd292d57ba29bc29b
V-Khvorostianyi <slavafh@gmail.com>
2018-02-07 07:41:41 +0200

    edit test

--- wallet_test.py
+++ wallet_test.py
@@ -6,8 +6,9 @@ from sandbox import *
 class WalletUnitTest(unittest.TestCase):
 
     def test_spend(self):
-        transaction = Transaction('10', 'cash(default)','test')
-        self.assertEqual(wallet.spend(wallet.account_list['cash(default)'], transaction.transaction_value), '-90.00',"not equal")
+        transaction = Transaction('10', 'test','test')
+        account = Account('test', '100')
+        self.assertEqual(wallet.spend(account.account_value, transaction.transaction_value), '90.00', "not equal")
 
 
 if __name__ == '__main__':
